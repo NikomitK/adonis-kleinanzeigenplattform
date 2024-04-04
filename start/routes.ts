5b1d2f3fb5621d35248f1e58765572d16c563a72
Niko <nikobo2003@gmail.com>
2024-04-04 16:44:31 +0200

    show details and implement db calls

--- start/routes.ts
+++ start/routes.ts
@@ -10,54 +10,74 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import db from '@adonisjs/lucid/services/db'
+
+/*const products = [
+    {
+        id: 1,
+        name: 'Fast neues Auto',
+        price: 100,
+        image: 'resources/images/auto.webp',
+        description: 'Hat ein paar kleine Kratzer und Dellen, aber das ist der Charakterdes Autos.'
+    },
+    {
+        id: 2,
+        name: 'Apple google',
+        price: 100,
+        image: 'resources/images/apple-google.jpg',
+        description: 'Apple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple google'
+    },
+    {
+        id: 3,
+        name: 'Alles',
+        price: 100,
+        image: 'resources/images/alles.jpg',
+        description: 'Ich verkaufe alles was existiert.'
+    },
+    {
+        id: 4,
+        name: 'Svenjamin',
+        price: 100,
+        negotiable: true,
+        shipping: true,
+        shippingPrice: 10,
+        image: 'resources/images/sven.jpg',
+        description: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
+    },
+    {
+        id: 5,
+        name: 'Apple google',
+        price: 100,
+        image: 'resources/images/apple-google.jpg',
+        description: 'Apple google'
+    },
+    {
+        id: 6,
+        name: 'Alles',
+        price: 100,
+        image: 'resources/images/alles.jpg',
+        description: 'Ich verkaufe alles was existiert.'
+    },
+    {
+        id: 7,
+        name: 'Svenjamin',
+        price: 100,
+        image: 'resources/images/sven.jpg',
+        description: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
+    },
+    {
+        id: 8,
+        name: 'Gniesbert',
+        price: 999999,
+        image: 'resources/images/gniesbert.png',
+        description: 'Mein scheiss Hund Gniesbert ist schon wieder in meinen Gaming pc geklettert, weshalb ich ihn verkaufen muss. Er ist 3 Jahre alt und hat schon 2 mal meinen Gaming pc zerstört. Ich verkaufe ihn für 999999€. Versand ist möglich, er läuft dann zu dir.'
+    }
+]*/
 
 router.get('/', async ({ view }) => {
-    const products = [
-        {
-            name: 'Fast neues Auto',
-            price: 100,
-            image: 'resources/images/auto.webp',
-            description: 'Hat ein paar kleine Kratzer und Dellen, aber das ist der Charakterdes Autos.'
-        },
-        {
-            name: 'Apple google',
-            price: 100,
-            image: 'resources/images/apple-google.jpg',
-            description: 'Apple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple google'
-        },
-        {
-            name: 'Alles',
-            price: 100,
-            image: 'resources/images/alles.jpg',
-            description: 'Ich verkaufe alles was existiert.'
-        },
-        {
-            name: 'Svenjamin',
-            price: 100,
-            image: 'resources/images/sven.jpg',
-            description: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
-        },
-        {
-            name: 'Apple google',
-            price: 100,
-            image: 'resources/images/apple-google.jpg',
-            description: 'Apple google'
-        },
-        {
-            name: 'Alles',
-            price: 100,
-            image: 'resources/images/alles.jpg',
-            description: 'Ich verkaufe alles was existiert.'
-        },
-        {
-            name: 'Svenjamin',
-            price: 100,
-            image: 'resources/images/sven.jpg',
-            description: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
-        }
-    ]
+    const products = await db.rawQuery('SELECT listing.*, image.path FROM listing, image WHERE listing.id = image.listing_id')
+    console.log(products)
     return view.render('pages/base', { page: 'pages/home', products })
-
 })
 
 router.get('/register', async ({ view }) => {
@@ -69,31 +89,14 @@ router.get('/login', async ({ view }) => {
 })
 
 router.get('/gespeichert', async ({ view }) => {
-    const gespeichert = [
-        {
-            name: 'Fast neues Auto',
-            price: 100,
-            image: 'https://placehold.co/150x100',
-            description: 'Hat ein paar kleine Kratzer und Dellen, aber das ist der Charakterdes Autos.'
-        },
-        {
-            name: 'Alles',
-            price: 100,
-            image: 'https://placehold.co/150x100',
-            description: 'Ich verkaufe alles was existiert.'
-        },
-        {
-            name: 'Svenjamin',
-            price: 100,
-            image: 'https://placehold.co/150x100',
-            description: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
-        },
-    ]
-    return view.render('pages/base', { page: 'pages/gespeichert', gespeichert})
+    const gespeichert = await db.rawQuery("SELECT listing.*, image.path FROM listing, image, saved WHERE listing.id = image.listing_id AND saved.username = 'nikomitk' AND saved.listing_id = listing.id")
+    console.log('gespeichert', gespeichert)
+    return view.render('pages/base', { page: 'pages/gespeichert', gespeichert })
 })
 
-router.get('/konto' , async ({ view }) => {
+router.get('/konto', async ({ view }) => {
     const konto = {
+        image: 'resources/images/sven.jpg',
         nutzername: 'Svenjamin',
         email: 'svenjamin@test.com',
         vorname: 'Sven',
@@ -114,22 +117,22 @@ router.get('/konto' , async ({ view }) => {
             description: 'Verkaufe einen Svenjamin',
             done: true,
             image: 'resources/images/achievments/sven.jpg'
-        },{
+        }, {
             title: 'Svenjamin',
             description: 'Verkaufe einen Svenjamin',
             done: true,
             image: 'resources/images/achievments/sven.jpg'
-        },{
+        }, {
             title: 'Svenjamin',
             description: 'Verkaufe einen Svenjamin',
             done: true,
             image: 'resources/images/achievments/sven.jpg'
-        },{
+        }, {
             title: 'Svenjamin',
             description: 'Verkaufe einen Svenjamin',
             done: false,
             image: 'resources/images/achievments/sven.jpg'
-        },{
+        }, {
             title: 'Svenjamin',
             description: 'Verkaufe einen Svenjamin',
             done: false,
@@ -140,20 +143,7 @@ router.get('/konto' , async ({ view }) => {
 })
 
 router.get('meine-anzeigen', async ({ view }) => {
-    const meineAnzeigen = [
-        {
-            name: 'Apple google',
-            price: 100,
-            image: 'https://placehold.co/150x100',
-            description: 'Apple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple googleApple google'
-        },
-        {
-            name: 'Gniesbert',
-            price: 999999,
-            image: 'resources/images/gniesbert.png',
-            description: 'Mein scheiss Hund Gniesbert ist schon wieder in meinen Gaming pc geklettert, weshalb ich ihn verkaufen muss. Er ist 3 Jahre alt und hat schon 2 mal meinen Gaming pc zerstört. Ich verkaufe ihn für 999999€. Versand ist möglich, er läuft dann zu dir.'
-        }
-    ]
+    const meineAnzeigen = await db.rawQuery("SELECT listing.*, image.path FROM listing, image WHERE listing.id = image.listing_id AND listing.username = 'nikomitk'")
     return view.render('pages/base', { page: 'pages/meine-anzeigen', meineAnzeigen })
 })
 
@@ -162,12 +152,26 @@ router.get('/anzeige-aufgeben', async ({ view }) => {
 })
 
 router.post('/anzeige-aufgeben', async ({ request, response }) => {
-    console.log(request.input('title'))
-    console.log(request.input('description'))
-    console.log(request.input('price'))
-    console.log(request.input('negotiable'))
-    console.log(request.input('shipping'))
-    console.log(request.input('shipping-price'))
-    //console.log(request.input('image'))
+    const title = request.input('title')
+    const description = request.input('description')
+    const price = request.input('price')
+
+    const result = await db.table('listing').insert({title, description, username: 'nikomitk', price })
+    const imageresult = await db.table('image').insert({ path: 'resources/images/sven.jpg', listing_id: result[0] })
     response.redirect('/meine-anzeigen')
+})
+
+router.get('/anzeige/:id', async ({ request, view }) => {
+    const anzeige = await db.rawQuery(`
+    SELECT listing.*, image.path 
+    FROM listing, image 
+    WHERE listing.id = image.listing_id 
+    AND listing.id = ${request.params().id}
+    LIMIT 1`)
+    return view.render('pages/base', { page: 'pages/anzeige', anzeige: anzeige[0] })
+})
+
+router.post('anzeige/save/:id', async ({ request, response }) => {
+    const result = await db.table('saved').insert({ listing_id: request.params().id, username: 'nikomitk' })
+    response.redirect('/gespeichert')
 })
