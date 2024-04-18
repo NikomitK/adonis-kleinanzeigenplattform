79c95834db431ed07d2e88341ca468f27f8ea44a
Niko <nikobo2003@gmail.com>
2024-04-18 15:16:21 +0200

    move card.js functions into app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,32 @@
-
-
 import.meta.glob(['../images/**'])
 import.meta.glob(['../images/icons/**'])
+
+function saveListing(id) {
+    console.log('clicked');
+    fetch(`/anzeige/${id}/save`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(response => {
+            response.status == 200 ? location.reload() : console.log('error');
+        }).catch(error => {
+            console.log('error');
+        });
+}
+
+function unsaveListing(id) {
+    console.log('clicked');
+    fetch(`/anzeige/${id}/unsave`,
+        {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(response => {
+            response.status == 200 ? location.reload() : console.log('error');
+        }).catch(error => {
+            console.log('error');
+        });
+} 
