66271a74c3a1a183372383a0c6e49d859a9de817
Niko <nikobo2003@gmail.com>
2024-04-23 09:34:33 +0200

    move navbar script into app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,37 @@
 import.meta.glob(['../images/**'])
 import.meta.glob(['../images/icons/**'])
 
+let navLinks = document.querySelectorAll('.nav-link-container a')
+navLinks.forEach(link => {
+    if (link.getAttribute('href') == window.location.pathname) {
+        link.ariaCurrent = 'page'
+        link.ariaDisabled = 'true'
+    }
+})
+
+
+let navbarToggler = document.getElementById('navbar-toggler')
+let mobileNavLinks = document.getElementById('mobile-nav-links')
+
+if (window.screen.width > 768) {
+    mobileNavLinks.style.display = "none"
+}
+
+navbarToggler.addEventListener('click', function () {
+    if (navbarToggler.getAttribute("aria-expanded") == "true") {
+        navbarToggler.setAttribute("aria-expanded", "false")
+        mobileNavLinks.style.height = "0px"
+        mobileNavLinks.classList.remove('shown')
+    } else {
+        navbarToggler.setAttribute("aria-expanded", "true")
+        mobileNavLinks.classList.add('shown')
+        mobileNavLinks.style.height = `calc(${mobileNavLinks.scrollHeight}px + 2em)`
+    }
+
+})
+
+
+// Ich weiß, nicht ganz best-practice, aber die alternative mit event listener per js wäre so unfassbar viel unleserlicher und im fall einer so kleinen website mit einem entwickler sind resultierende naming collisions eher ein kleineres problem 
 window.saveListing = function saveListing(id) {
     console.log('clicked');
     fetch(`/anzeige/${id}/save`,
