c6c3562f97b854088a7fc56365b472e8e3dbeae1
Niko <nikobo2003@gmail.com>
2024-05-05 12:21:59 +0200

    minor anzeige aufgeben fixes

--- app/validators/listing.ts
+++ app/validators/listing.ts
@@ -2,7 +2,7 @@ import vine from '@vinejs/vine'
 
 export const lisitingFormValidator = vine.compile(
     vine.object({
-        title: vine.string().trim().minLength(5).maxLength(80),
+        title: vine.string().trim().minLength(4).maxLength(80),
         description: vine.string().trim().maxLength(1000).minLength(50),
         price: vine.number().min(0.00).transform((value) => {
             return value.toFixed(2).toString()
