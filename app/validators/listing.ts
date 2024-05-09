1202f748838a0f1cee8f2a32865da433144ceb70
Nikolas Bodenmüller <nikobo2003@gmail.com>
2024-05-09 10:41:25 +0200

    change minlength of listing description

--- app/validators/listing.ts
+++ app/validators/listing.ts
@@ -3,7 +3,7 @@ import vine from '@vinejs/vine'
 export const lisitingFormValidator = vine.compile(
     vine.object({
         title: vine.string().trim().minLength(4).maxLength(80),
-        description: vine.string().trim().maxLength(1000).minLength(50),
+        description: vine.string().trim().maxLength(1000).minLength(10),
         price: vine.number().min(0.00).transform((value) => {
             return value.toFixed(2).toString()
         }),
